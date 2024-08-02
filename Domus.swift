632fefaf0f80c0339466e7cc7b9f2dde0b34ca07
Tanish Rana <120825361+thetanishrana@users.noreply.github.com>
2024-08-02 09:23:10 +0530

    Add files via upload

---
+++ Domus.swift
@@ -0,0 +1,17 @@
+//
+//  Domus.swift
+//  Domus
+//
+//  Created by Tanish Rana on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DomusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
