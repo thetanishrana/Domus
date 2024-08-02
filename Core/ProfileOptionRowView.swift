63b584b6d1e439354c271d7303911e32b3598711
Tanish Rana <120825361+thetanishrana@users.noreply.github.com>
2024-08-02 09:57:55 +0530

    Add files via upload

---
+++ Core/ProfileOptionRowView.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileOptionRowView.swift
+//  Domus
+//
+//  Created by Tanish Rana on 01/08/24.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    let imageName: String
+    let title: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: imageName)
+                    .fontWeight(.semibold)
+                
+                Text(title)
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+            }
+            
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    ProfileOptionRowView(imageName: "gear", title: "Settings")
+}
---
+++ Core/ProfileView.swift
@@ -0,0 +1,59 @@
+//
+//  ProfileView.swift
+//  Domus
+//
+//  Created by Tanish Rana on 01/08/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            // Profile Login
+            VStack (alignment: .leading, spacing: 32) {
+                VStack (alignment: .leading, spacing: 8) {
+                    Text("Profile")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                    
+                    Text("Log in to rent your home")
+                }
+                
+                Button {
+                    print("Log In")
+                } label: {
+                    Text("Log In")
+                        .foregroundStyle(.white)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 48)
+                        .background(.red)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                
+                HStack {
+                    Text("Don't have an account?")
+                    
+                    Text("Sign Up")
+                        .fontWeight(.semibold)
+                        .underline()
+                }
+                .font(.caption)
+            }
+            
+            VStack (spacing: 24) {
+                ProfileOptionRowView(imageName: "gear", title: "Settings")
+                ProfileOptionRowView(imageName: "accessibility", title: "Accessibility")
+                ProfileOptionRowView(imageName: "questionmark.circle", title: "FAQs")
+            }
+            .padding()
+        }
+        
+       
+    }
+}
+
+#Preview {
+    ProfileView()
+}
