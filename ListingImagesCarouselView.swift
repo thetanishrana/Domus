7f9e9679c5f4628710910ceaf9004a2174b9f14c
Tanish Rana <120825361+thetanishrana@users.noreply.github.com>
2024-08-02 09:48:27 +0530

    Add files via upload

---
+++ ListingImagesCarouselView.swift
@@ -0,0 +1,27 @@
+//
+//  ListingImagesCarouselView.swift
+//  Domus
+//
+//  Created by Tanish Rana on 01/08/24.
+//
+
+import SwiftUI
+
+struct ListingImagesCarouselView: View {
+    let listing: Listing
+    
+    var body: some View {
+        TabView {
+            ForEach(listing.imageURLs, id: \.self) {image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+            }
+        }
+        .tabViewStyle(.page)
+    }
+}
+
+#Preview {
+    ListingImagesCarouselView(listing: DeveloperPreview.shared.listings[0])
+}
