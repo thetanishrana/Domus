5c46e47efcea680616296bd614ad093a62c5a41d
Tanish Rana <120825361+thetanishrana@users.noreply.github.com>
2024-08-02 15:23:54 +0530

    Update Search-Filter.swift

--- Core/Explore/View/Search-Filter.swift
+++ Core/Explore/View/Search-Filter.swift
@@ -1,155 +1,39 @@
 //
-//  DestinationSearchView.swift
+//  ExploreViewModel.swift
 //  Domus
 //
-//  Created by Tanish Rana on 01/08/24.
+//  Created by Tanish Rana on 02/06/24.
 //
 
-import SwiftUI
+import Foundation
 
-enum DestinationSearchOptions {
-    case location
-    case bedrooms
-}
-
-struct DestinationSearchView: View {
-    @Binding var show: Bool
-    @ObservedObject var viewModel: ExploreViewModel
-    @State private var selectedOption: DestinationSearchOptions = .location
-    @State private var numBedrooms = 1
+class ExploreViewModel: ObservableObject {
+    @Published var listings = [Listing]()
+    @Published var searchedLocation = ""
+    private let service: ExploreService
+    private var listingCopy = [Listing]()
     
-    var body: some View {
-        VStack {
-            HStack {
-                Button {
-                    withAnimation(.snappy) {
-                        viewModel.updateListingsForLocation()
-                        show.toggle()
-                    }
-                } label: {
-                    Image(systemName: "xmark.circle")
-                        .imageScale(.large)
-                        .foregroundStyle(.black)
-                }
-                
-                Spacer()
-                
-                if !viewModel.searchedLocation.isEmpty {
-                    Button("Clear") {
-                        viewModel.searchedLocation = ""
-                        viewModel.updateListingsForLocation()
-                    }
-                    .foregroundStyle(.black)
-                    .font(.subheadline)
-                    .fontWeight(.semibold)
-                }
-            }
-            .padding()
-            
-            VStack (alignment: .leading) {
-                if selectedOption == .location {
-                    Text("Where is your next home?")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                    
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                            .imageScale(.small)
-                        
-                        TextField("Search Location", text: $viewModel.searchedLocation)
-                            .font(.subheadline)
-                            .onSubmit {
-                                viewModel.updateListingsForLocation()
-                                show.toggle()
-                            }
-                    }
-                    .frame(height: 44)
-                    .padding(.horizontal)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(lineWidth: 1)
-                            .foregroundStyle(Color(.systemGray4))
-                    }
-                } else {
-                    ExtractedView(title: "Where", description: "Search")
-                }
-                
-            }
-            .modifier(collapsibleDestinationViewModifier())
-            .frame(height: selectedOption == .location ? 120 :64)
-            .onTapGesture {
-                withAnimation(.snappy) {
-                    selectedOption = .location
-                }
-            }
-            
-            // bedrooms
-            VStack (alignment: .leading) {
-                if selectedOption == .bedrooms {
-                    Text("How many bedrooms?")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        
-                    Stepper {
-                        Text("\(numBedrooms) Bedrooms")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                    } onIncrement: {
-                        numBedrooms += 1
-                    } onDecrement: {
-                        guard numBedrooms > 1 else {return}
-                        numBedrooms -= 1
-                    }
-                } else {
-                    ExtractedView(title: "Bedrooms", description: "Add")
-                }
-            }
-            .modifier(collapsibleDestinationViewModifier())
-            .frame(height: selectedOption == .bedrooms ? 120 : 64)
-            .onTapGesture {
-                withAnimation(.snappy) {
-                    selectedOption = .bedrooms
-                }
-            
-            }
-        }
+    init(service: ExploreService) {
+        self.service = service
         
-        Spacer()
+        Task { await fetchListings() }
     }
-}
-
-#Preview {
-    DestinationSearchView(show: .constant(false), viewModel: ExploreViewModel(service: ExploreService()))
-}
-
-struct collapsibleDestinationViewModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .padding()
-            .background(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .padding()
-            .shadow(radius: 10)
-    }
-}
-
-struct ExtractedView: View {
-    let title: String
-    let description: String
     
-    var body: some View {
-        VStack {
-            HStack {
-                Text(title)
-                    .foregroundStyle(.gray)
-                
-                Spacer()
-                
-                Text(description)
-            }
-            .fontWeight(.semibold)
-            .font(.subheadline)
-            
+    func fetchListings() async {
+        do {
+            self.listings = try await service.fetchListings()
+            self.listingCopy = listings
+        } catch {
+            print("DEBUG: Failed to fetch listings with error: \(error.localizedDescription)")
         }
+    }
+    
+    func updateListingsForLocation() {
+        let filteredListings = listings.filter({
+            $0.city.lowercased() == searchedLocation.lowercased() ||
+            $0.state.lowercased() == searchedLocation.lowercased()
+        })
+        
+        self.listings = filteredListings.isEmpty ? listingCopy : filteredListings
     }
 }
