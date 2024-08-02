2f9cd4bade0e7f3934cd6885d643258c9995f286
Tanish Rana <120825361+thetanishrana@users.noreply.github.com>
2024-08-02 10:05:10 +0530

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Domus
+//
+//  Created by Tanish Rana on 01/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabBar()
+    }
+}
+
+#Preview {
+    ContentView()
+}
