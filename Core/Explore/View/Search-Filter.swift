691ef01b36a10c11c76be780ff21ee80bf800050
Tanish Rana <120825361+thetanishrana@users.noreply.github.com>
2024-08-02 15:23:31 +0530

    Update Search-Filter.swift

--- Core/Explore/View/Search-Filter.swift
+++ Core/Explore/View/Search-Filter.swift
@@ -1,48 +1,155 @@
 //
-//  Search-Filter.swift
+//  DestinationSearchView.swift
 //  Domus
 //
-//  Created by Tanish Rana on 01/05/24.
+//  Created by Tanish Rana on 01/08/24.
 //
 
 import SwiftUI
 
-struct Search_Filter: View {
-    @Binding var location: String
+enum DestinationSearchOptions {
+    case location
+    case bedrooms
+}
+
+struct DestinationSearchView: View {
+    @Binding var show: Bool
+    @ObservedObject var viewModel: ExploreViewModel
+    @State private var selectedOption: DestinationSearchOptions = .location
+    @State private var numBedrooms = 1
     
     var body: some View {
-        HStack {
-            Image(systemName: "magnifyingglass")
-            
-            VStack(alignment: .leading, spacing: 2) {
-                Text(location.isEmpty ? "Where is home?" : location)
-                    .font(.footnote)
-                    .fontWeight(.semibold)
+        VStack {
+            HStack {
+                Button {
+                    withAnimation(.snappy) {
+                        viewModel.updateListingsForLocation()
+                        show.toggle()
+                    }
+                } label: {
+                    Image(systemName: "xmark.circle")
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
+                }
                 
-                Text(location.isEmpty ? "Anywhere" : "results")
-                    .font(.caption2)
-                    .foregroundStyle(.gray)
+                Spacer()
+                
+                if !viewModel.searchedLocation.isEmpty {
+                    Button("Clear") {
+                        viewModel.searchedLocation = ""
+                        viewModel.updateListingsForLocation()
+                    }
+                    .foregroundStyle(.black)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                }
+            }
+            .padding()
+            
+            VStack (alignment: .leading) {
+                if selectedOption == .location {
+                    Text("Where is your next home?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        
+                        TextField("Search Location", text: $viewModel.searchedLocation)
+                            .font(.subheadline)
+                            .onSubmit {
+                                viewModel.updateListingsForLocation()
+                                show.toggle()
+                            }
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
+                } else {
+                    ExtractedView(title: "Where", description: "Search")
+                }
+                
+            }
+            .modifier(collapsibleDestinationViewModifier())
+            .frame(height: selectedOption == .location ? 120 :64)
+            .onTapGesture {
+                withAnimation(.snappy) {
+                    selectedOption = .location
+                }
             }
             
-            Spacer()
+            // bedrooms
+            VStack (alignment: .leading) {
+                if selectedOption == .bedrooms {
+                    Text("How many bedrooms?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        
+                    Stepper {
+                        Text("\(numBedrooms) Bedrooms")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                    } onIncrement: {
+                        numBedrooms += 1
+                    } onDecrement: {
+                        guard numBedrooms > 1 else {return}
+                        numBedrooms -= 1
+                    }
+                } else {
+                    ExtractedView(title: "Bedrooms", description: "Add")
+                }
+            }
+            .modifier(collapsibleDestinationViewModifier())
+            .frame(height: selectedOption == .bedrooms ? 120 : 64)
+            .onTapGesture {
+                withAnimation(.snappy) {
+                    selectedOption = .bedrooms
+                }
             
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Image(systemName: "line.3.horizontal.decrease.circle")
-                    .foregroundStyle(.black)
-            })
+            }
         }
-        .padding(.horizontal)
-        .padding(.vertical, 10)
-        .overlay{
-            Capsule()
-                .stroke(lineWidth: 0.5)
-                .foregroundStyle(Color(.systemGray4))
-                .shadow(color: .black.opacity(0.4), radius: 2)
-        }
-        .padding()
+        
+        Spacer()
     }
 }
 
 #Preview {
-    Search_Filter(location: .constant("Brooklyn"))
+    DestinationSearchView(show: .constant(false), viewModel: ExploreViewModel(service: ExploreService()))
+}
+
+struct collapsibleDestinationViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+    }
+}
+
+struct ExtractedView: View {
+    let title: String
+    let description: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .foregroundStyle(.gray)
+                
+                Spacer()
+                
+                Text(description)
+            }
+            .fontWeight(.semibold)
+            .font(.subheadline)
+            
+        }
+    }
 }
