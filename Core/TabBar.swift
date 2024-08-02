420ddbd1df2997ffaee2836c4645b172ed518eba
Tanish Rana <120825361+thetanishrana@users.noreply.github.com>
2024-08-02 09:59:40 +0530

    Add files via upload

---
+++ Core/TabBar.swift
@@ -0,0 +1,36 @@
+//
+//  TabBar.swift
+//  Domus
+//
+//  Created by Tanish Rana on 01/08/24.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            ExploreView()
+                .tabItem {
+                    Label("Explore", systemImage: "house")
+                        .foregroundColor(.black)
+                }
+            
+            ShortlistView()
+                .tabItem {
+                    Label("Shortlisted", systemImage: "bookmark")
+                        .foregroundColor(.black)
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                        .foregroundColor(.black)
+                }
+        }
+    }
+}
+
+#Preview {
+    TabBar()
+}
